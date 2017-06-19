2a79f5bf5671a6768ed8daf4d66405bb1473c2e9
enthusiastic geek <enthusiasticgeek@gmail.com>
2017-06-19 17:54:20 -0400

    Update digital_input.c

--- digital_input.c
+++ digital_input.c
@@ -9,10 +9,13 @@ int main(void) {
    /* Digital Output Pin 13 onboard LED on Arduino Mega 2560*/
    DDRB |= _BV(PB7);
    /* Digital Input Pin 6 on Arduino Mega 2560*/
-   /* Pull up resistor 10K Ohms between 5V and one end push button switch; 
+   DDRH &= ~(_BV(PH3));
+   /*Internal input pullup resistor high*/
+   PORTH |= (1<<PH3); 
+   /* IF above is not done then external Pull up resistor 10K Ohms between 5V and one end push button switch; 
    connect the same end of push button switch to input pin 6
    and connect the other end of push button switch to GND*/
-   DDRH &= ~(_BV(PH3));
+   
    for(;;){
         // While Button is pressed, LED is on
         if (bit_is_clear(PINH, PH3)) {

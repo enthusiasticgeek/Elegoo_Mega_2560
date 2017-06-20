865a95078b79171982aacc22c6c3b964f7b0ae0b
enthusiastic geek <enthusiasticgeek@gmail.com>
2017-06-19 20:39:02 -0400

    Update digital_input.c

--- digital_input.c
+++ digital_input.c
@@ -1,5 +1,7 @@
 //(c) 2017 Pratik M Tambe <enthusiasticgeek@gmail.com>
 //This is to turn ON/OFF the Elegoo onboard LED when an input is applied
+//Connect digital pin 6 to a push button switch's one end 
+//while connecting GND to another end of push button switch (no need of external pull-up or pull-down resistors)
 
 #include <avr/io.h>
 #include <util/delay.h>

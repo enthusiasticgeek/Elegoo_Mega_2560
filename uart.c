20eb1f50af0bd13f8e71f8b9ccfb9a9e2be3c8d3
pratik.tambe@brivo.com <pratik.tambe@brivo.com>
2017-10-14 14:15:25 -0400

    uart tx tested to work

--- uart.c
+++ uart.c
@@ -1,7 +1,11 @@
-//#define F_CPU 16000000UL  // 16 MHz
+//(c) 2017 Pratik M Tambe <enthusiasticgeek@gmail.com>
+// Tested with WINGONEER USB 2.0 to TTL UART 6PIN CP2102 Module Serial Converter
+// Connect RX1 pin of the AVR to TX pin of the serial converter 
+// Connect TX1 pin of the AVR to RX pin of the serial converter 
+// Connect GND pin of the AVR to GND pin of the serial converter 
 
 /*Very Important - change F_CPU to match target clock 
-  Note: default AVR CLKSEL is 1MHz internal RC
+  Note: default AVR clock is 16MHz external crystal
   This program transmits continously on USART. Interrupt is used for 
 	Receive charactor, which is then transmitted instead. LEDs are used 
 	as a test. Normal RX routine is included but not used.
@@ -17,10 +21,11 @@
 #endif
 
 // Define baud rate
-#define USART_BAUDRATE 38400   
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+#define USART_BAUDRATE 115200   
+//#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+#define BAUD_PRESCALE (unsigned int)( F_CPU / (16.0 * USART_BAUDRATE) - 0.5 )
 
-volatile unsigned char value;  
+unsigned char value;  
 /* This variable is volatile so both main and RX interrupt can use it.
 It could also be a uint8_t type */
 
@@ -40,16 +45,16 @@ void USART_Init(void){
    // Set baud rate
    /* Load upper 8-bits into the high byte of the UBRR register
       Default frame format is 8 data bits, no parity, 1 stop bit
-      to change use UCSRC, see AVR datasheet*/ 
-   UBRR1L = (uint8_t) (BAUD_PRESCALE);// Load lower 8-bits into the low byte of the UBRR register
-   UBRR1H = (uint8_t) (BAUD_PRESCALE >> 8); 
+      to change use UCSRC, see AVR datasheet */ 
    // Enable receiver and transmitter and receive complete interrupt 
-   UCSR1B = ((1<<TXEN1)|(1<<RXEN1)|(0<<UCSZ12)|(1<<RXCIE1));
+   UCSR1B = (1<<TXEN1)|(1<<RXEN1)|(0<<UCSZ12)|(1<<RXCIE1);//|(1<<TXCIE1);
    //8-bit data, 1 stop bit, Aynchronous USART, no parity	
-   UCSR1C = (1<<UCSZ11)|(1<<UCSZ10)|(0<<USBS2)|(0<<UMSEL11)|(0<<UMSEL10)|(0<<UPM11)|(0<<UPM10);
+   UCSR1C = (1<<UCSZ11)|(1<<UCSZ10)|(0<<USBS1)|(0<<UMSEL11)|(0<<UMSEL10)|(0<<UPM11)|(0<<UPM10);
+   UBRR1H = (uint8_t) (BAUD_PRESCALE >> 8); 
+   UBRR1L = (uint8_t) (BAUD_PRESCALE);// Load lower 8-bits into the low byte of the UBRR register
 }
 
-void USART_SendByte(uint8_t u8Data){
+void USART_SendByte(unsigned char u8Data){
   // Wait until last byte has been transmitted
   while((UCSR1A &(1<<UDRE1)) == 0);
   // Transmit data
@@ -63,9 +68,8 @@ uint8_t USART_ReceiveByte(){
 }
 
 void Led_init(void){
-   //outputs, all off
-   DDRB = 0xFF;       
-   PORTB = 0xFF;        
+   /* set pin 7 of PORTB for output*/
+   DDRB |= _BV(DDB7);
 }
 
 int main(void){
@@ -74,10 +78,11 @@ int main(void){
    sei();          // enable all interrupts
    Led_init();     // init LEDs for testing
    value = 'A';    // 0x41;    
-   PORTB = ~value; // 0 = LED on
-   
+   //PORTB = ~value; // 0 = LED on
    for(;;){                  // Repeat indefinitely
+     PORTB = PORTB ^ _BV(PORTB7);// toggle built in LED on TX
      USART_SendByte(value);  // send value 
-     _delay_ms(250);         // delay just to stop minicom screen cluttering    
+     _delay_ms(550);         // delay just to stop minicom screen cluttering    
    }
 }
+

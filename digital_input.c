465c03ee158add470e1bfc9f6cefae5e41ec6bac
enthusiastic geek <enthusiasticgeek@gmail.com>
2017-10-31 14:56:04 -0400

    Update digital_input.c

--- digital_input.c
+++ digital_input.c
@@ -1,4 +1,4 @@
-//(c) 2017 Pratik M Tambe <enthusiasticgeek@gmail.com>
+//Copyright (c) 2017 Pratik M Tambe <enthusiasticgeek@gmail.com>
 //This is to turn ON/OFF the Elegoo onboard LED when an input is applied
 //Connect digital pin 6 to a push button switch's one end 
 //while connecting GND to another end of push button switch (no need of external pull-up or pull-down resistors)
